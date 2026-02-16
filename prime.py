a133e08c607c7c1299549b7d8522f478c644c962
Sukhveer Dhaliwal <kaursukhveer21@gmail.com>
2026-02-16 11:56:28 -0500

    Step2 -Returns[]
    Assert that the number 1 Generates an Empty List []

--- prime.py
+++ prime.py
@@ -11,8 +11,10 @@ def test_non_integer_raises_ValueError():
     with pytest.raises(ValueError):
         generate_prime_factors([1,1])
 
+def test_1_Returns_Empty_List():
+    """Step 2: Assert that the number generates an Empty List[]"""
+    assert generate_prime_factors(1) == []
 
 def generate_prime_factors(n):
     if not isinstance(n, int):
         raise ValueError('Input must be an integer')
-    return []
