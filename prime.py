bea7241a90053783532992a50f1d131de14902cc
Sukhveer Dhaliwal <kaursukhveer21@gmail.com>
2026-02-16 11:48:16 -0500

    Step1-ValueError
    Non-Integer Value raises ValueError

--- prime.py
+++ prime.py
@@ -1,3 +1,18 @@
 """
 prime.py -- Write the application code here
 """
+import pytest
+
+def test_non_integer_raises_ValueError():
+    with pytest.raises(ValueError):
+        generate_prime_factors("A String")
+    with pytest.raises(ValueError):
+        generate_prime_factors(2.3)
+    with pytest.raises(ValueError):
+        generate_prime_factors([1,1])
+
+
+def generate_prime_factors(n):
+    if not isinstance(n, int):
+        raise ValueError('Input must be an integer')
+    return []
